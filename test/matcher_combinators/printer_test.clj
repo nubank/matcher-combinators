2c21f6e9a6c2735c2fd73ec4a8ef03c414b607b4
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-09-23 18:25:59 -0300

    Improve printing tests

--- test/matcher_combinators/printer_test.clj
+++ test/matcher_combinators/printer_test.clj
@@ -3,153 +3,71 @@
             [matcher-combinators.printer :as printer]
             [matcher-combinators.model :as model]
             [matcher-combinators.core :as core]
-            [clojure.test.check.properties :as prop]
             [clojure.test.check.generators :as gen]
-            [clojure.test.check :as tc]
-            [midje.emission.state :as m-state]
-            [midje.emission.api :as emission]
-            [midje.parsing.1-to-explicit-form.facts :as parse-facts]
-            [midje.util.pile :as pile]
-            [midje.emission.plugins.util :as util]
-            [clojure.string :as str]
-            [clojure.string :as string])
-  (:import [java.io StringWriter]))
+            [midje-generative :refer [for-all *midje-generative-runs*]]
+            [colorize.core :as colorize]
+            [clojure.pprint :as pprint]))
 
-(def gen-atom (gen/one-of [gen/int
-                           gen/string
-                           gen/symbol
-                           gen/symbol-ns
-                           gen/keyword
-                           gen/boolean
-                           gen/ratio
-                           gen/char]))
+(def simple-double (gen/double* {:infinite? false
+                                 :NaN?      false}))
 
-(def gen-seq (gen/vector gen-atom))
+(def scalars (gen/one-of [gen/int gen/large-integer simple-double gen/char-ascii gen/string-ascii gen/ratio gen/boolean
+                          gen/keyword gen/keyword-ns gen/symbol gen/symbol-ns gen/uuid]))
+(def any (gen/recursive-gen gen/container-type scalars))
 
-(def gen-map (gen/map gen-atom gen-atom))
+(binding [*midje-generative-runs* 100]
 
-(def gen-coll (gen/one-of [gen-seq gen-map]))
-
-(def gen-clojure-expression (gen/one-of [gen-coll gen-atom]))
-
-(def prop1
-  (prop/for-all [elem gen-clojure-expression]
-    (= (printer/markup-expression elem) elem)))
-
-(defmacro silently [& forms]
-  `(let [output-counters-before# (m-state/output-counters)
-         writer# (new StringWriter)]
-     (binding [clojure.test/*test-out* writer#]
-       (let [result# (do ~@forms)]
-         (m-state/set-output-counters! output-counters-before#)
-         result#))))
-
-(resetting-midje-counters (fact 1 => 2))
-
-(defmacro expand-fact-with-substitutions [substitutions form]
-  (let [quoted-subs (into {} (map (fn [[k v]] [`(quote ~(symbol (name k))) v]) substitutions))]
-    (midje.parsing.1-to-explicit-form.metadata/with-wrapped-metadata {:midje/table-bindings quoted-subs}
-                                                                     (macroexpand (parse-facts/wrap-fact-around-body {} form)))))
-
-
-(expand-fact-with-substitutions {x 1 y 2} (fact 1 => 22))
-
-
-(facts "scalar values act as equals-value matchers"
   (facts "on how we markup expressions that need special coloring"
-    #_(fact "regular clojure expressions are not marked-up at all"
-        (tc/quick-check 100 prop1) => (contains {:result true}))
+    (fact "regular clojure expressions are not marked-up at all"
+      (for-all [expression any]
+        (printer/markup-expression expression) => expression))
 
-    ))
+    (fact "Mismatches are marked up in yellow and red"
+      (printer/markup-expression (model/->Mismatch 1 2))
+      => (list 'mismatch
+               (printer/->ColorTag :yellow 1)
+               (printer/->ColorTag :red 2))
 
-; copied from midje.emission.plugins.util
-(defn- format-binding-map [binding-map]
-  (let [formatted-entries (for [[k v] binding-map]
-                            (str (pr-str k) " " (pr-str v)))]
-    (str "[" (string/join "\n                        " formatted-entries) "]")))
+      (for-all [expected any
+                actual   any]
+        (printer/markup-expression (model/->Mismatch expected actual))
+        => (list 'mismatch
+                 (printer/->ColorTag :yellow expected)
+                 (printer/->ColorTag :red actual))))
 
-(defn- log-error [names values]
-  #_(when (emission/config-above? :print-nothing) (#'emission/bounce-to-plugin :fail-generative {}))
-  (util/emit-one-line (str "With generated values: "
-                           (format-binding-map (zipmap names values)))))
+    (fact "Missing values are marked up in red"
+      (printer/markup-expression (model/->Missing 42))
+      => (list 'missing
+               (printer/->ColorTag :red 42))
 
-(fact "regular clojure expressions are not marked-up at all"
-  (let [fact-fn (fn [elem] (fact elem => integer?))
-        prop    (prop/for-all* [gen/int]
-                  fact-fn)
-        run     (silently (tc/quick-check 10 prop))]
-    (if (:result run)
-      (emission/pass)
-      (do (log-error '(elem) (-> run nu/tap :shrunk :smallest))
-        (apply fact-fn (-> run :shrunk :smallest))))))
+      (for-all [expected any]
+        (printer/markup-expression (model/->Missing expected))
+        => (list 'missing
+                 (printer/->ColorTag :red expected))))
 
+    (fact "Unexpected values are also marked up in red"
+      (printer/markup-expression (model/->Unexpected 42))
+      => (list 'unexpected
+               (printer/->ColorTag :red 42))
 
-(midje.emission.state/reset-output-counters!)
-(midje.emission.state/output-counters)
+      (for-all [actual any]
+        (printer/markup-expression (model/->Unexpected actual))
+        => (list 'unexpected
+                 (printer/->ColorTag :red actual)))))
 
+  (midje.config/with-augmented-config {:partial-prerequisites true}
+    (facts "On printing"
+      (fact "When an expression can be marked up, will use color tags to print in color"
+        (printer/print ..markupable..)
+        => (str "(foo " (colorize/red "bar") ")" "\n")
+        (provided
+          (printer/markup-expression ..markupable..)
+          => (list 'foo (printer/->ColorTag :red 'bar))))
 
-(def ^:dynamic *midje-generative-runs* 10)
+      (fact "When printing a regular expression, just pprint it"
+        (printer/print {:aaaaaaaaaaaa [100000000 100000000 100000000 100000000 100000000]
+                        :bbbbbbbbbbbb [200000000 200000000 200000000 200000000 200000000]})
+        => "{:aaaaaaaaaaaa [100000000 100000000 100000000 100000000 100000000],\n :bbbbbbbbbbbb [200000000 200000000 200000000 200000000 200000000]}\n"
 
-(defmacro for-all [& forms]
-  (let [bindings    (->> forms first (partition 2))
-        checks      (rest forms)
-        prop-names  (mapv first bindings)
-        prop-values (mapv second bindings)]
-    `(let [fact-fn# (fn ~prop-names (fact ~@checks))
-           prop#    (prop/for-all* ~prop-values fact-fn#)
-           run#     (silently (tc/quick-check *midje-generative-runs* prop#))]
-       (if (:result run#)
-         (emission/pass)
-         (do (log-error '~prop-names (-> run# :shrunk :smallest))
-             (apply fact-fn# (-> run# :shrunk :smallest)))))))
-
-(for-all [strictly-pos gen/s-pos-int
-          any-integer  gen/int]
-  (fact "Summing an integer to a positive integer should be positive? Really?"
-    strictly-pos => integer?
-    {:x (+ strictly-pos any-integer)} => (contains {:x pos?})))
-
-
-(fact "regular clojure expressions are not marked-up at all"
-  )
-
-(tabular
-  (fact ?x => 1)
-  ?x
-  1
-  2)
-
-(macroexpand-1 `(fact "huh" 4220 => 1337))
-
-(midje.checking.checkables/check-one
-  (fact "huh" 4220 => 1337) nil)
-
-(comment
-  (property "regular clojure expressions are not marked-up at all"
-            (prop/for-all [elem gen-clojure-expression]
-              (printer/markup-expression elem) => elem)))
-
-(comment
-  (println
-    (printer/print {:a 10}))
-
-  (println
-    (printer/print {:a (model/->Mismatch 10 20)}))
-
-
-
-
-  (println
-    (printer/print
-      #matcher_combinators.model.Mismatch{:expected [#matcher_combinators.core.Value{:expected 1}
-                                                     #matcher_combinators.core.Value{:expected 2}
-                                                     #matcher_combinators.core.Value{:expected 3}],
-                                          :actual   [1 2]}))
-
-  (println
-    (printer/print
-      (second
-        (core/match
-          (core/in-any-order [1 2 3])
-          [1 2])
-        ))))
+        (for-all [exp any]
+          (printer/print exp) => (with-out-str (pprint/pprint exp)))))))
