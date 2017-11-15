9d0c7864e621bcd9102382cc33f6ebcba563656d
Phillip Mates <phillip.mates@nubank.com.br>
2017-11-15 10:44:11 +0100

    cleanup project file

--- project.clj
+++ project.clj
@@ -1,20 +1,15 @@
 (defproject matcher-combinators "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for creating matcher combinator to compare nested data structures"
+  :url "https://github.com/rafaeldff/matcher-combinators"
   :license {:name "Proprietary"}
 
-  :repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]
-                 ["central" {:url "http://repo1.maven.org/maven2/" :snapshots false}]
+  :repositories [["central" {:url "http://repo1.maven.org/maven2/" :snapshots false}]
                  ["clojars" {:url "https://clojars.org/repo/"}]]
 
   :plugins [[lein-midje "3.1.3"]
             [s3-wagon-private "1.3.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [;[ns-tracker "0.3.0"]
-                                  [org.clojure/tools.namespace "0.2.11"]
-
-                                  [midje "1.9.0-alpha12" :exclusions [org.clojure/clojure]]
-                                  ;[org.clojure/java.classpath "0.2.3"]
+  :profiles {:dev {:dependencies [[colorize "0.1.1" :exclusions [org.clojure/clojure]]
                                   [org.clojure/test.check "0.9.0"]
-                                  ;[nu-test "0.3.4"]
-                                  [colorize "0.1.1" :exclusions [org.clojure/clojure]]]}})
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [midje "1.9.0-alpha12" :exclusions [org.clojure/clojure]]]}})
