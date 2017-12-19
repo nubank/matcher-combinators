1f1679dd1e5d63447f25f53cc8c073597b9de92a
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-12-19 20:05:52 -0200

    Use APLv2 License

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject matcher-combinators "0.1.0-SNAPSHOT"
   :description "Library for creating matcher combinator to compare nested data structures"
   :url "https://github.com/rafaeldff/matcher-combinators"
-  :license {:name "Proprietary"}
+  :license {:name "Apache License, Version 2.0"}
 
   :repositories [["central" {:url "https://repo1.maven.org/maven2/" :snapshots false}]
                  ["clojars" {:url "https://clojars.org/repo/"}]]
