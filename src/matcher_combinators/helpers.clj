9c95b8cd72c63aaa0c4e2929ad95cb182f17b1fa
Phillip Mates <phillip.mates@nubank.com.br>
2018-01-29 06:49:48 -0200

    Fix in-any-order to work with identical matchers (#8)
    * fix in-any-order issue with identical matchers
    
    * add 'double' to parser generative tests
    
    * fix issue with interpreting funcs as matchers w/o parser ns
    
    * version bump
    
    * helpers tests
    
    * cleaner remove-first impl

--- src/matcher_combinators/helpers.clj
+++ src/matcher_combinators/helpers.clj
@@ -8,3 +8,9 @@
   [coll]
   (for [i (range 0 (count coll))]
     (lazy-cat (drop i coll) (take i coll))))
+
+(defn remove-first
+  "Similar to `remove` but stops after removing 1 element"
+  [pred coll]
+  (let [[x [y & z]] (split-with (complement pred) coll)]
+    (concat x z)))
