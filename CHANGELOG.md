cfd6694c2539afcb39429d063e3dce6f51def841
Phillip Mates <phillip.mates@nubank.com.br>
2018-09-13 22:55:36 +0200

    version bump to get project working with cljdoc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## [0.3.1]
+- fix to get project working with cljdoc
+
 ## [0.3.0]
 - regexes are now interpreted as matchers
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/matcher-combinators "0.3.0"
+(defproject nubank/matcher-combinators "0.3.1"
   :description "Library for creating matcher combinator to compare nested data structures"
   :url "https://github.com/nubank/matcher-combinators"
   :license {:name "Apache License, Version 2.0"}
