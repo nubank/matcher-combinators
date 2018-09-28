0e92e960b260d4e913ddee2b635493ec6a556ef5
Phillip Mates <phillip.mates@nubank.com.br>
2018-09-28 16:07:47 +0200

    add comments, remove expound

--- src/matcher_combinators/result.clj
+++ src/matcher_combinators/result.clj
@@ -1,9 +1,14 @@
 (ns matcher-combinators.result
-  (:require [clojure.spec.alpha :as s]
-            [expound.alpha :as expound]))
+  (:require [clojure.spec.alpha :as s]))
 
+;; the weight of the mismatch. `0` is a match, and any number above is the
+;; number of leaf matchers that mismatch
 (s/def ::weight nat-int?)
+
 (s/def ::type #{:mismatch :match})
+
+;; either the original value, when matching, or the value with mismatches
+;; annotated
 (s/def ::value any?)
 
 (s/def ::result (s/keys :req [::weight ::type ::annotated-result]))
@@ -13,4 +18,4 @@
 
 (defn assert-spec [spec value]
   (when-not (s/valid? spec value)
-    (throw (AssertionError. (expound/expound-str spec value)))))
+    (throw (AssertionError. (s/explain-data spec value)))))
