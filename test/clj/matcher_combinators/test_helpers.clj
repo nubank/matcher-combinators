209dba1348580dfd2c0f11b9517b5ad9b85bf3f8
David Chelimsky <dchelimsky@gmail.com>
2020-05-31 20:23:01 -0500

    docstring for test helper

--- test/clj/matcher_combinators/test_helpers.clj
+++ test/clj/matcher_combinators/test_helpers.clj
@@ -10,6 +10,13 @@
   (spec.test/unstrument))
 
 (def gen-any-equatable
+  "Generates from gen/any-equatable such that there is no set in the
+  resulting structure that contains the value `false`. This is to get
+  around a bug which causes `(match? #{false} #{false})` to return
+  false.  Until that is fixed, use this generator instead of
+  gen/any-equatable.
+
+  See https://github.com/nubank/matcher-combinators/issues/124"
   (gen/such-that
    (fn [v]
      (every? (fn [node] (or (not (set? node))
